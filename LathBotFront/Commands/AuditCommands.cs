efeb529885b8f76ac3e3de7e3270591023fd9510
JulianusIV <65790187+JulianusIV@users.noreply.github.com>
2021-05-27 19:13:34 +0200

    basic audit commands

--- LathBotFront/Commands/AuditCommands.cs
+++ LathBotFront/Commands/AuditCommands.cs
@@ -7,6 +7,12 @@ using DSharpPlus.CommandsNext.Attributes;
 using LathBotBack.Repos;
 using LathBotBack.Config;
 using LathBotBack.Models;
+using DSharpPlus;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Collections.Generic;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
 
 namespace LathBotFront.Commands
 {
@@ -34,31 +40,66 @@ namespace LathBotFront.Commands
 		}
 
 		[Command("audit")]
-		[RequireRoles(RoleCheckMode.Any, "Bot Management")]
 		public async Task Audit(CommandContext ctx)
 		{
+			await ctx.RespondAsync(DoAudit(ctx, ctx.Member));
+		}
+
+		[Command("Audit")]
+		public async Task Audit(CommandContext ctx, DiscordMember mod)
+		{
+			await ctx.RespondAsync(DoAudit(ctx, mod));
+		}
+
+		[Command("auditall")]
+		[RequireUserPermissions(Permissions.Administrator)]
+		public async Task AuditAll(CommandContext ctx)
+		{
+			var members = await ctx.Guild.GetAllMembersAsync();
+			var mods = members.Where(x => x
+				.PermissionsIn(ctx.Guild.GetChannel(700350465174405170))
+				.HasPermission(Permissions.KickMembers) && !x.IsBot);
+
+			var pages = new List<Page>();
+			foreach (var item in mods)
+			{
+				var builder = DoAudit(ctx, item);
+				if (builder.Embed == null)
+				{
+					await ctx.RespondAsync(builder);
+					return;
+				}
+				pages.Add(new Page { Embed = builder.Embed });
+			}
+			await ctx.Channel.SendPaginatedMessageAsync(ctx.Member, pages);
+		}
+
+		private DiscordMessageBuilder DoAudit(CommandContext ctx, DiscordMember mod)
+		{
+			if (!mod.PermissionsIn(ctx.Guild.GetChannel(700350465174405170)).HasPermission(Permissions.KickMembers))
+			{
+				return new DiscordMessageBuilder { Content = "Member is not a mod (anymore)." };
+			}
 			UserRepository urepo = new UserRepository(ReadConfig.configJson.ConnectionString);
 			AuditRepository repo = new AuditRepository(ReadConfig.configJson.ConnectionString);
-			bool result = urepo.GetIdByDcId(ctx.Member.Id, out int id);
+			bool result = urepo.GetIdByDcId(mod.Id, out int id);
 			if (!result)
 			{
-				await ctx.RespondAsync("Error getting the user from the database");
-				return;
+				return new DiscordMessageBuilder { Content = $"Error getting user {mod.Id} from the database" };
 			}
 			result = repo.Read(id, out Audit audit);
 			if (!result)
 			{
-				await ctx.RespondAsync("Error getting the audit from the database");
-				return;
+				return new DiscordMessageBuilder { Content = $"Error getting an audit for {mod.Id} from the database" };
 			}
 			DiscordEmbedBuilder builder = new DiscordEmbedBuilder
 			{
 				Title = "Moderator:",
-				Description = $"{ctx.Member.DisplayName}#{ctx.Member.Discriminator} ({ctx.Member.Id})",
-				Color = ctx.Member.Color,
+				Description = $"{mod.DisplayName}#{mod.Discriminator} ({mod.Id})",
+				Color = mod.Color,
 				Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail
 				{
-					Url = ctx.Member.AvatarUrl
+					Url = mod.AvatarUrl
 				}
 			};
 			builder.AddField("Warn Amount:", audit.Warns.ToString());
@@ -67,7 +108,7 @@ namespace LathBotFront.Commands
 			builder.AddField("Unmute Amount:", audit.Unmutes.ToString());
 			builder.AddField("Kick Amount:", audit.Kicks.ToString());
 			builder.AddField("Ban Amount:", audit.Bans.ToString());
-			await ctx.RespondAsync(builder);
+			return new DiscordMessageBuilder { Embed = builder.Build() };
 		}
 	}
 }
