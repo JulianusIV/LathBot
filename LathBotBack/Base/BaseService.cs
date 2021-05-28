7b38036a58277ea81e2051998777d698d3aacf05
JulianusIV <65790187+JulianusIV@users.noreply.github.com>
2021-05-28 19:05:59 +0200

    Added BaseService

---
+++ LathBotBack/Base/BaseService.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus;
+
+namespace LathBotBack.Base
+{
+	public abstract class BaseService<T> where T : class, new()
+	{
+		#region Singleton
+		private static T instance;
+		private static readonly object padlock = new object();
+		public static T Instance
+		{
+			get
+			{
+				lock (padlock)
+				{
+					if (instance != null)
+						instance = new T();
+					return instance;
+				}
+			}
+		}
+		#endregion
+
+		public abstract void Init(DiscordClient client);
+	}
+}
