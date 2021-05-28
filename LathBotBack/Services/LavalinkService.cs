6dbd53f72b1f8731820405a0d6962582297475e8
JulianusIV <65790187+JulianusIV@users.noreply.github.com>
2021-05-28 19:43:33 +0200

    Added LavalinkService

---
+++ LathBotBack/Services/LavalinkService.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.Lavalink;
+using LathBotBack.Base;
+using LathBotBack.Enums;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LathBotBack.Services
+{
+	class LavalinkService : BaseService<LavalinkService>
+	{
+		public Dictionary<DiscordGuild, List<LavalinkTrack>> Queues;
+		public Dictionary<DiscordGuild, Repeaters> Repeats;
+
+		public override void Init(DiscordClient client) { }
+	}
+}
