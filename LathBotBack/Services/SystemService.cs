e3764ff6b461e3f80986f861400fa27767ff2465
JulianusIV <65790187+JulianusIV@users.noreply.github.com>
2021-05-28 19:49:39 +0200

    Added SystemService

---
+++ LathBotBack/Services/SystemService.cs
@@ -0,0 +1,22 @@
+﻿using DSharpPlus;
+using LathBotBack.Base;
+using LathBotBack.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace LathBotBack.Services
+{
+	class SystemService : BaseService<SystemService>
+	{
+		public Timer WarnTimer = new Timer(3600000);
+
+		public LoggingPublisher Logger = new LoggingPublisher();
+
+		public override void Init(DiscordClient client)
+		{
+			WarnTimer.Start();
+		}
+	}
+}
