1848149fef893d61fcb2913613dd9664317b22dc
JulianusIV <ivjulianus@gmail.com>
2022-04-17 01:29:05 +0200

    Use Process diagnostics for start time

--- LathBotBack/Services/StartupService.cs
+++ LathBotBack/Services/StartupService.cs
@@ -1,40 +1,39 @@
-﻿using System;
-
-using DSharpPlus;
-
+﻿using DSharpPlus;
 using LathBotBack.Base;
+using System;
+using System.Diagnostics;
 
 namespace LathBotBack.Services
 {
-	public class StartupService : BaseService
-	{
-		#region Singleton
-		private static StartupService instance;
-		private static readonly object padlock = new object();
-		public static StartupService Instance
-		{
-			get
-			{
-				lock (padlock)
-				{
-					if (instance == null)
-						instance = new StartupService();
-					return instance;
-				}
-			}
-		}
-		#endregion
+    public class StartupService : BaseService
+    {
+        #region Singleton
+        private static StartupService instance;
+        private static readonly object padlock = new object();
+        public static StartupService Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                        instance = new StartupService();
+                    return instance;
+                }
+            }
+        }
+        #endregion
 
-		public bool IsInDesignMode { get; private set; }
-		public bool StartUpCompleted = false;
-		public bool InitCompleted = false;
-		public DateTime StartTime { get; set; }
+        public bool StartUpCompleted = false;
+        public bool InitCompleted = false;
+        public DateTime StartTime
+        {
+            get => Process.GetCurrentProcess().StartTime;
+        }
 
-		public override void Init(DiscordClient client)
-		{
-			StartTime = DateTime.Now;
-			IsInDesignMode = false;
-			StartUpCompleted = true;
-		}
-	}
+        public override void Init(DiscordClient client)
+        {
+            StartUpCompleted = true;
+        }
+    }
 }
