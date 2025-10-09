4b607955fe3bc39d9849a235d84e265c1bb8e45e
JulianusIV <ivjulianus@gmail.com>
2025-10-09 18:54:17 +0200

    add missing } (oops™)

--- LathBotFront/OnTimerMethods.cs
+++ LathBotFront/OnTimerMethods.cs
@@ -230,27 +230,27 @@ namespace LathBotFront
         //            await DiscordObjectService.Instance.APODChannel.SendMessageAsync(builder);
         //        }
         //    }
-        //}
-
-        //struct APODJsonObject
-        //{
-        //    [JsonProperty("copyright")]
-        //    public string Copyright { get; set; }
-        //    [JsonProperty("date")]
-        //    public string Date { get; set; }
-        //    [JsonProperty("explanation")]
-        //    public string Explanation { get; set; }
-        //    [JsonProperty("hdurl")]
-        //    public string HdUrl { get; set; }
-        //    [JsonProperty("media_type")]
-        //    public string MediaType { get; set; }
-        //    [JsonProperty("service_version")]
-        //    public string ServiceVersion { get; set; }
-        //    [JsonProperty("thumbnail_url")]
-        //    public string ThumbnailUrl { get; set; }
-        //    [JsonProperty("title")]
-        //    public string Title { get; set; }
-        //    [JsonProperty("url")]
-        //    public string URL { get; set; }
-        //}
     }
+
+    //struct APODJsonObject
+    //{
+    //    [JsonProperty("copyright")]
+    //    public string Copyright { get; set; }
+    //    [JsonProperty("date")]
+    //    public string Date { get; set; }
+    //    [JsonProperty("explanation")]
+    //    public string Explanation { get; set; }
+    //    [JsonProperty("hdurl")]
+    //    public string HdUrl { get; set; }
+    //    [JsonProperty("media_type")]
+    //    public string MediaType { get; set; }
+    //    [JsonProperty("service_version")]
+    //    public string ServiceVersion { get; set; }
+    //    [JsonProperty("thumbnail_url")]
+    //    public string ThumbnailUrl { get; set; }
+    //    [JsonProperty("title")]
+    //    public string Title { get; set; }
+    //    [JsonProperty("url")]
+    //    public string URL { get; set; }
+    //}
+}
